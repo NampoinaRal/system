659666d84ac7ac1aa92d9326a1e5eb926c136ed3
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-04-24 13:48:25 +0300

    Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,10 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      -- intelephense = {},
-      hyprls = {},
-    },
-  },
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			-- intelephense = {},
+			hyprls = {},
+			ts_ls = {},
+		},
+	},
 }
