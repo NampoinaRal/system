b195e76c4f25d0b8be5433eb8a04456ca212075c
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-13 10:30:55 +0300

    Update .config/hypr/hyprland.lua

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -1,20 +1,9 @@
-source = ~/.config/hypr/colors.conf
 require("colors")
-source = ~/.config/hypr/programs.conf
 require("programs")
-source = ~/.config/hypr/monitors.conf
 require("monitors")
-source = ~/.config/hypr/autostart.conf
 require("autostart")
-source = ~/.config/hypr/env.conf
 require("env")
-source = ~/.config/hypr/permissions.conf
 require("permissions")
-source = ~/.config/hypr/design.conf
 require("design")
-source = ~/.config/hypr/keybinding.conf
 require("keybinding")
-source = ~/.config/hypr/workspaces.conf
 require("workspace")
-source = ~/.config/hypr/input.conf
-require("input")
