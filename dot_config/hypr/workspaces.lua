012f7011453ce4febd2bd3077e000d36a798f260
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-13 12:40:15 +0300

    Update .config/hypr/workspaces.lua

--- dot_config/hypr/workspaces.lua
+++ dot_config/hypr/workspaces.lua
@@ -1,39 +1,49 @@
-##############################
-### WINDOWS AND WORKSPACES ###
-##############################
+--------------------------------
+---- WINDOWS AND WORKSPACES ----
+--------------------------------
 
-# See https://wiki.hypr.land/Configuring/Window-Rules/ for more
-# See https://wiki.hypr.land/Configuring/Workspace-Rules/ for workspace rules
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 
-# Example windowrules that are useful
+-- Example window rules that are useful
 
-windowrule {
-    # Ignore maximize requests from all apps. You'll probably like this.
-    name = suppress-maximize-events
-    match:class = .*
+local suppressMaximizeRule = hl.window_rule({
+	-- Ignore maximize requests from all apps. You'll probably like this.
+	name = "suppress-maximize-events",
+	match = { class = ".*" },
 
-    suppress_event = maximize
-}
+	suppress_event = "maximize",
+})
+-- suppressMaximizeRule:set_enabled(false)
 
-windowrule {
-    # Fix some dragging issues with XWayland
-    name = fix-xwayland-drags
-    match:class = ^$
-    match:title = ^$
-    match:xwayland = true
-    match:float = true
-    match:fullscreen = false
-    match:pin = false
+hl.window_rule({
+	-- Fix some dragging issues with XWayland
+	name = "fix-xwayland-drags",
+	match = {
+		class = "^$",
+		title = "^$",
+		xwayland = true,
+		float = true,
+		fullscreen = false,
+		pin = false,
+	},
 
-    no_focus = true
-}
+	no_focus = true,
+})
 
-# Hyprland-run windowrule
-windowrule {
-    name = move-hyprland-run
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--     name  = "no-anim-overlay",
+--     match = { namespace = "^my-overlay$" },
+--     no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
 
-    match:class = hyprland-run
+-- Hyprland-run windowrule
+hl.window_rule({
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
 
-    move = 20 monitor_h-120
-    float = yes
-}
+	move = "20 monitor_h-120",
+	float = true,
+})
