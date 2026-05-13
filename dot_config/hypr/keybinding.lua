6c73d7ea6df8ce05ee57fd4a32e865daa7a74317
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-13 12:31:32 +0300

    Update .config/hypr/keybinding.lua

--- dot_config/hypr/keybinding.lua
+++ dot_config/hypr/keybinding.lua
@@ -1,87 +1,60 @@
-###################
-### KEYBINDINGS ###
-###################
+-------------------
+--- KEYBINDINGS ---
+-------------------
 
-# See https://wiki.hypr.land/Configuring/Keywords/
-$mainMod = SUPER # Sets "Windows" key as main modifier
+local mainMod = "SUPER"
+hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + C", hl.dsp.window.close())
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
+hl.bind(mainMod .. " + spqce", hl.dsp.exec_cmd(menu))
+hl.bind(mainMod .. " + F", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(
+	mainMod .. " + V",
+	hl.dsp.exec_cmd(
+		"clipvault list | awk -F '\t' '{print $1 " - " $2}' | tofi | cut -d '-' -f1 | clipvault get | wl-copy"
+	)
+)
+hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
+hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))
 
-# Example binds, see https://wiki.hypr.land/Configuring/Binds/ for more
-bind = $mainMod, Return, exec, $terminal
-bind = $mainMod, C, killactive,
-# bind = $mainMod, M, exec, command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch exit
-bind = $mainMod, E, exec, $fileManager
-bind = $mainMod, B, exec, firefox
-bind = $mainMod, space, exec, $menu
+hl.bind(mainMod .. " + left", hl.dsp.focus({ direction = "left" }))
+hl.bind(mainMod .. " + right", hl.dsp.focus({ direction = "right" }))
+hl.bind(mainMod .. " + up", hl.dsp.focus({ direction = "up" }))
+hl.bind(mainMod .. " + down", hl.dsp.focus({ direction = "down" }))
 
-bind = $mainMod, F, togglefloating,
-bind = $mainMod, V, exec, clipvault list | awk -F '\t' '{print $1 "-" $2}' | tofi | cut -d '-' -f1 | clipvault get | wl-copy
-bind = $mainMod, P, pseudo, # dwindle
-bind = $mainMod, BackSpace, layoutmsg, swapsplit
-bind = $mainMod, J, layoutmsg, togglesplit # dwindle
-bind = $mainMod, L, exec, hyprctl keyword general:layout scrolling
-bind = $mainMod, D, exec, hyprctl keyword general:layout dwindle
+hl.bind(mainMod .. " + S", hl.dsp.workspace.toggle_special("magic"))
+hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
 
-bind = ,PRINT,exec,hyprshot -m output
-bind = $mainMod,PRINT, exec, hyprshot -m window
-bind = $mainMod SHIFT, PRINT, exec, hyprshot -m region
+hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
+hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
 
-# Move focus with mainMod + arrow keys
-bind = $mainMod, left, movefocus, l
-bind = $mainMod, right, movefocus, r
-bind = $mainMod, up, movefocus, u
-bind = $mainMod, down, movefocus, d
+hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind(mainMod .. " + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
-# Switch workspaces with mainMod + [0-9]
-bind = $mainMod, code:87, workspace, 1
-bind = $mainMod, code:88, workspace, 2
-bind = $mainMod, code:89, workspace, 3
-bind = $mainMod, code:83, workspace, 4
-bind = $mainMod, code:84, workspace, 5
-bind = $mainMod, code:85, workspace, 6
-bind = $mainMod, code:79, workspace, 7
-bind = $mainMod, code:80, workspace, 8
-bind = $mainMod, code:81, workspace, 9
-bind = $mainMod, code:90, workspace, 10
+hl.bind(
+	"XF86AudioRaiseVolume",
+	hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioLowerVolume",
+	hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+	{ locked = true, repeating = true }
+)
+hl.bind(
+	"XF86AudioMicMute",
+	hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"),
+	{ locked = true, repeating = true }
+)
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%+"), { locked = true, repeating = true })
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl -e4 -n2 set 5%-"), { locked = true, repeating = true })
 
-# Move active window to a workspace with mainMod + SHIFT + [0-9]
-bind = $mainMod SHIFT, code:87, movetoworkspace, 1
-bind = $mainMod SHIFT, code:88, movetoworkspace, 2
-bind = $mainMod SHIFT, code:89, movetoworkspace, 3
-bind = $mainMod SHIFT, code:83, movetoworkspace, 4
-bind = $mainMod SHIFT, code:84, movetoworkspace, 5
-bind = $mainMod SHIFT, code:85, movetoworkspace, 6
-bind = $mainMod SHIFT, code:79, movetoworkspace, 7
-bind = $mainMod SHIFT, code:80, movetoworkspace, 8
-bind = $mainMod SHIFT, code:81, movetoworkspace, 9
-bind = $mainMod SHIFT, code:90, movetoworkspace, 10
-
-# Example special workspace (scratchpad)
-bind = $mainMod, S, togglespecialworkspace, magic
-bind = $mainMod SHIFT, S, movetoworkspace, special:magic
-
-# Scroll through existing workspaces with mainMod + scroll
-bind = $mainMod, mouse_down, workspace, e+1
-bind = $mainMod, mouse_up, workspace, e-1
-
-bind = $mainMod SHIFT, left, workspace, e+1
-bind = $mainMod SHIFT, right, workspace, e-1
-
-# Move/resize windows with mainMod + LMB/RMB and dragging
-bindm = $mainMod, mouse:272, movewindow
-bindm = $mainMod, mouse:273, resizewindow
-
-# Laptop multimedia keys for volume and LCD brightness
-bindel = ,XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+
-bindel = ,XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-
-bindel = ,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-bindel = ,XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
-bindel = ,XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+
-bindel = ,XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-
-
-# Requires playerctl
-bindl = , XF86AudioNext, exec, playerctl next
-bindl = , XF86AudioPause, exec, playerctl play-pause
-bindl = , XF86AudioPlay, exec, playerctl play-pause
-bindl = , XF86AudioPrev, exec, playerctl previous
-
-
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"), { locked = true })
+hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
