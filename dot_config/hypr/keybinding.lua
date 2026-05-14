b2e0c3b6e352e7f4d9b8428df0a74fd68a59f299
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-14 11:02:18 +0300

    Update .config/hypr/keybinding.lua

--- dot_config/hypr/keybinding.lua
+++ dot_config/hypr/keybinding.lua
@@ -56,10 +56,8 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"), { locked = true })
 
-hl.bind(mainMod .. "+ KP_7", hl.dsp.focus({ workspace = 7 }))
+hl.bind(mainMod .. "+ key:87", hl.dsp.focus({ workspace = 1 }))
+hl.bind(mainMod .. "+ SHIFT + key:87", hl.dsp.window.move({ workspace = 1 }))
 
-for i = 1, 10 do
-	local key = "KP_" .. i % 10 -- 10 maps to key 0
-	hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
-	hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
-end
+hl.bind(mainMod .. "+ key:88", hl.dsp.focus({ workspace = 2 }))
+hl.bind(mainMod .. "+ SHIFT + key:88", hl.dsp.window.move({ workspace = 2 }))
