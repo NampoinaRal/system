4e73ed3fda48f3bf97e52a452ae997020701f3be
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-20 13:49:55 +0300

    Update .config/hypr/input.lua

--- dot_config/hypr/input.lua
+++ dot_config/hypr/input.lua
@@ -11,11 +11,21 @@ hl.gesture({
 hl.config({
 	input = {
 		kb_layout = "us",
-		numlock_by_default = true,
 		follow_mouse = 1,
 
 		touchpad = {
 			natural_scroll = true,
 		},
 	},
+})
+
+hl.device({
+	name = "at-translated-set-2-keyboard",
+	kb_layout = "fr",
+	numlock_by_default = true,
+})
+
+hl.device({
+	name = "atom-he68-pro-1-keyboard",
+	kb_layout = "us",
 })
