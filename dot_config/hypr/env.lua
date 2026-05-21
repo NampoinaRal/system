6c1fb45571e634f7b5fa5b06ceeaefd2411b65da
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-21 10:59:55 +0300

    Update .config/hypr/env.lua

--- dot_config/hypr/env.lua
+++ dot_config/hypr/env.lua
@@ -5,6 +5,6 @@
 hl.env("XCURSOR_SIZE", "40")
 hl.env("HYPRCURSOR_SIZE", "40")
 hl.env("HYPRCURSOR_THEME", "volantes")
-hl.env("SSH_AUTH_SOCK", "$XDG_RUNTIME_DIR/ssh-agent.socket")
+hl.env("SSH_AUTH_SOCK", os.getenv("XDG_RUNTIME_DIR") .. "/ssh-agent.socket")
 hl.env("QT_QPA_PLATFORMTHEME", "hyprqt6engine")
 hl.env("TERMINAL", "kitty")
