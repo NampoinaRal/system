ba616bb553540c969270f6c0447e26c6f32d0d47
Nampoina Razafindralaisa <nampoina@mety-aby.com>
2026-05-30 18:24:52 +0300

    Update .config/hypr/keybinding.lua

--- dot_config/hypr/keybinding.lua
+++ dot_config/hypr/keybinding.lua
@@ -9,7 +9,7 @@ local menu = "tofi-drun"
 
 hl.bind(mainMod .. " + Return", hl.dsp.exec_cmd(terminal))
 hl.bind(mainMod .. " + C", hl.dsp.window.close())
-hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager .. '"/home/nampoina/$(ls -a /home/nampoina/ | tofi)"'))
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd('thunar "/home/nampoina/$(ls -a /home/nampoina/ | tofi)"'))
 hl.bind(mainMod .. " + space", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + SHIFT + space", hl.dsp.exec_cmd("tofi-run | bash"))
 hl.bind(mainMod .. " + F", hl.dsp.window.float({ action = "toggle" }))
